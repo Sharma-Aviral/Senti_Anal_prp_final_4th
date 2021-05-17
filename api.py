c7b6675306db29b433d675df73c4965165711833
unknown <aviral.sharma.012@gmail.com>
2021-05-17 19:14:18 +0530

    adding tempelate engine and overcomplicating a rather very simple project

--- api.py
+++ api.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)    
