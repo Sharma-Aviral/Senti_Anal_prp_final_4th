fadb786b3db42c95766ca62f8d067d46ab46abcf
unknown <aviral.sharma.012@gmail.com>
2021-05-18 21:11:08 +0530

    commit

--- api.py
+++ api.py
@@ -9,3 +9,16 @@ def hello_world():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)    
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+if __name__ == '__main__':
+   app.run(debug = True)    
+    
