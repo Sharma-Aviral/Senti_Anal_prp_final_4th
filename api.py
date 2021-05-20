84306e063a01e30c2ca69a75f9c5cf486ba25d84
unknown <aviral.sharma.012@gmail.com>
2021-05-20 15:23:02 +0530

    brython

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
+import senti
 
-app = Flask(__name__ ,static_url_path="/static",static_folder="/static")
+# app = Flask(__name__ ,static_url_path="/static",static_folder="/static")
 
-@app.route("/")
-def index(): 
- return render_template("index.html")
+# @app.route("/")
+# def index(): 
+#  return render_template("index.html")
+
+senti.twitter_anal("BeerBicesdasdpsGuy" , 10000)
--- senti.py
+++ senti.py
@@ -15,8 +15,7 @@ global count_Neut
 count_Neut = 0
 
 #info_initilisation
-tweeterUser = "kunalkamra88"
-number_oF_tweets = 100000
+
 
 #getting access tokkens and api key form .env
 project_folder = os.path.expanduser('~/Desktop/projects/python/senti')  
@@ -36,7 +35,7 @@ except tweepy.TweepError:
     print('Error! Failed to get request token.')
 
 
-#textblob senti analysis check
+#textblob senti analysis checkt
 def sentimenti(sent):
     testimonial = TextBlob(sent)
     if testimonial.sentiment.polarity > 0 :
@@ -53,23 +52,28 @@ def sentimenti(sent):
         global count_Neut 
         count_Neut = count_Neut +1
 
-        
-user = api.get_user(tweeterUser)
-print(user.followers_count)
-for friend in user.friends():
-   print(friend.screen_name)    
+#exporting function // calling tweepy and cheking the user
+def twitter_anal(tweeterUser , number_oF_tweets):     
+    try:
+     user = api.get_user(tweeterUser)
+    except:
+      print("User Does not Exist") 
+    print("No of followers:"+str(user.followers_count))
+   
+    for friend in user.friends():
+        print(friend.screen_name)    
 
+    tweets = api.user_timeline(tweeterUser)
+    for info in tweets[:number_oF_tweets-1]:
+        print("ID: {}".format(info.id))
+        print(info.created_at)
+        print(info.text)
+        sentimenti(info.text)
+        print("\n")
 
-tweets = api.user_timeline(tweeterUser)
-for info in tweets[:number_oF_tweets-1]:
-     print("ID: {}".format(info.id))
-     print(info.created_at)
-     print(info.text)
-     sentimenti(info.text)
-     print("\n")
+    y = np.array([count_Pos,count_Neg,count_Neut])
+    mylabels = ["Positive", "Negative", "Neutral"]
 
-y = np.array([count_Pos,count_Neg,count_Neut])
-mylabels = ["Positive", "Negative", "Neutral"]
+    plt.pie(y, labels = mylabels, startangle = 90)
+    plt.show() 
 
-plt.pie(y, labels = mylabels, startangle = 90)
-plt.show() 
